1470fb72f9919d08375ff62859eb29b446f23089
Darren Tsung <darren@onesignal.com>
2019-08-24 12:16:30 -0700

    Add test for children from TicTacToeState

--- game-tree/src/games/tic_tac_toe.rs
+++ game-tree/src/games/tic_tac_toe.rs
@@ -1,6 +1,6 @@
 use crate::GameTreeNode;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Player {
     X = 0,
     O = 1,
@@ -15,6 +15,7 @@ impl Player {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct TicTacToeState {
     /// The state of a game of tic-tac-toe can be
     /// represented as an length-9 array of slot states (integers).
@@ -45,5 +46,79 @@ impl GameTreeNode for TicTacToeState {
         }
 
         child_nodes
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn slot(s: Option<char>) -> Option<Player> {
+        match s.expect("char exists") {
+            'O' => Some(Player::O),
+            'X' => Some(Player::X),
+            ' ' => None,
+            c => panic!("Unexpected input for slot(..) - got '{}'", c),
+        }
+    }
+
+    fn board(row_0: &str, row_1: &str, row_2: &str) -> [Option<Player>; 9] {
+        debug_assert_eq!(row_0.len(), 3);
+        debug_assert_eq!(row_1.len(), 3);
+        debug_assert_eq!(row_2.len(), 3);
+
+        let mut board = [None; 9];
+        board[0] = slot(row_0.chars().nth(0));
+        board[1] = slot(row_0.chars().nth(1));
+        board[2] = slot(row_0.chars().nth(2));
+        board[3] = slot(row_1.chars().nth(0));
+        board[4] = slot(row_1.chars().nth(1));
+        board[5] = slot(row_1.chars().nth(2));
+        board[6] = slot(row_2.chars().nth(0));
+        board[7] = slot(row_2.chars().nth(1));
+        board[8] = slot(row_2.chars().nth(2));
+        board
+    }
+
+    #[test]
+    fn children_works_as_expected_for_tictactoe_state() {
+        #[rustfmt::skip]
+        let initial_board = board(
+            " XX",
+            "OOX",
+            " XO"
+        );
+
+        let initial_state = TicTacToeState {
+            board: initial_board,
+            current_player: Player::X,
+        };
+
+        let children = initial_state.children();
+        assert_eq!(children.len(), 2, "expected 2 possible new states");
+
+        #[rustfmt::skip]
+        assert_eq!(children[0],
+            TicTacToeState {
+                board: board(
+                    "XXX",
+                    "OOX",
+                    " XO",
+                ),
+                current_player: Player::O,
+            }
+        );
+
+        #[rustfmt::skip]
+        assert_eq!(children[1],
+            TicTacToeState {
+                board: board(
+                    " XX",
+                    "OOX",
+                    "XXO",
+                ),
+                current_player: Player::O,
+            }
+        );
     }
 }
