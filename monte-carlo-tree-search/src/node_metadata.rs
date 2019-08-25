1200f5c8f777fc34dbd5682aaadf38f049d9a3cf
Darren Tsung <darren@onesignal.com>
2019-08-25 07:39:35 -0700

    Use log2 instead of log10 for exploration component

--- monte-carlo-tree-search/src/node_metadata.rs
+++ monte-carlo-tree-search/src/node_metadata.rs
@@ -40,7 +40,7 @@ impl NodeMetadata {
 
         let exploitation_component = total_reward / number_of_visits;
         let exploration_component =
-            exploration_factor * (number_of_visits.log10() / parent_number_of_visits).sqrt();
+            exploration_factor * (number_of_visits.log2() / parent_number_of_visits).sqrt();
 
         exploitation_component + exploration_component
     }
