7b0fd8bbeefa4a8eaa30d15408acbd29b6a2d9b1
Darren Tsung <darren@onesignal.com>
2019-08-25 10:36:00 -0700

    Cargo formatting on compare-strategies

--- compare-strategies/src/main.rs
+++ compare-strategies/src/main.rs
@@ -1,14 +1,17 @@
-use structopt::{self, StructOpt};
-use std::fmt;
-use strum_macros::EnumString;
-use game_tree_strategy::Strategy;
+use game_tree::{GameTreeNode, NodeState};
 use game_tree_strategy::strategies::random::RandomStrategy;
 use game_tree_strategy::strategies::search_tree::LearningSearchTreeStrategy;
-use game_tree::{NodeState, GameTreeNode};
+use game_tree_strategy::Strategy;
 use std::collections::HashMap;
+use std::fmt;
+use structopt::{self, StructOpt};
+use strum_macros::EnumString;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "compare-strategies", about = "A CLI tool to help compare game tree strategies.")]
+#[structopt(
+    name = "compare-strategies",
+    about = "A CLI tool to help compare game tree strategies."
+)]
 struct Opt {
     #[structopt(short = "s", long = "strategy")]
     pub strategy_to_compare: StrategyType,
@@ -19,7 +22,7 @@ struct Opt {
 
 #[derive(Debug, EnumString)]
 enum StrategyType {
-    #[strum(serialize="MCTS")]
+    #[strum(serialize = "MCTS")]
     MonteCarloSearchTree,
 }
 
@@ -40,18 +43,23 @@ fn main() {
 fn main_ty<N: GameTreeNode<Node = N> + 'static>(opt: Opt, root_node: N) {
     match opt.strategy_to_compare {
         StrategyType::MonteCarloSearchTree => {
-            let iterations_per_search = 1_000;
+            let iterations_per_search = 10_000;
             let exploration_factor = 2.0_f64.sqrt();
-            let strategies = vec![
-                LearningSearchTreeStrategy::new(root_node.clone(), iterations_per_search, exploration_factor)
-            ];
+            let strategies = vec![LearningSearchTreeStrategy::new(
+                root_node.clone(),
+                iterations_per_search,
+                exploration_factor,
+            )];
 
             run_games(root_node, strategies);
-        },
+        }
     }
 }
 
-fn run_games<N: GameTreeNode<Node = N> + 'static, S: fmt::Display + Strategy<N>>(root_node: N, strategies: Vec<S>) {
+fn run_games<N: GameTreeNode<Node = N> + 'static, S: fmt::Display + Strategy<N>>(
+    root_node: N,
+    strategies: Vec<S>,
+) {
     let random_strategy = RandomStrategy;
     for strategy in strategies {
         let mut total_reward = 0;
@@ -67,22 +75,28 @@ fn run_games<N: GameTreeNode<Node = N> + 'static, S: fmt::Display + Strategy<N>>
                         total_reward += reward;
                         *reward_counts.entry(reward).or_insert(0) += 1;
                         break;
-                    },
+                    }
                     NodeState::HasChildren(children) => {
                         current = if current.is_self_turn() {
                             strategy.select_child(current, children)
                         } else {
                             random_strategy.select_child(current, children)
                         };
-                    },
+                    }
                 }
             }
         }
 
-        let mut reward_counts_strings = reward_counts.into_iter().map(|(reward, count)| {
-            format!("{} - {}", reward, count)
-        }).collect::<Vec<_>>();
+        let mut reward_counts_strings = reward_counts
+            .into_iter()
+            .map(|(reward, count)| format!("{} - {}", reward, count))
+            .collect::<Vec<_>>();
         reward_counts_strings.sort();
-        println!("{} - total={} ({})", strategy, total_reward, reward_counts_strings.join(", "));
+        println!(
+            "{} - total={} ({})",
+            strategy,
+            total_reward,
+            reward_counts_strings.join(", ")
+        );
     }
 }
