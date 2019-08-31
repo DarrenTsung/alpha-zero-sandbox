54a1f379fcbaeb0edd5eedf35dc53306a3dba53e
Darren Tsung <darren@onesignal.com>
2019-08-31 07:30:12 -0700

    Increase benchmark to 1000 iters

--- monte-carlo-tree-search/benches/search_tree_benchmark.rs
+++ monte-carlo-tree-search/benches/search_tree_benchmark.rs
@@ -7,12 +7,12 @@ use game_tree::games::tic_tac_toe::TicTacToeState;
 use monte_carlo_tree_search::{SearchConfig, SearchTree};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("SearchTree - TicTacToe - 100 iters", |b| b.iter(|| {
+    c.bench_function("SearchTree - TicTacToe - 1,000 iters", |b| b.iter(|| {
         let root = TicTacToeState::new();
         let tree = SearchTree::new();
         black_box(tree.search(root, SearchConfig {
             max_duration: Duration::from_secs(60),
-            max_iterations: 100,
+            max_iterations: 1_000,
 
             exploration_factor: 2.0_f64.sqrt(),
         }));
